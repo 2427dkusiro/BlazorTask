9f159103feb3d1ceaed3466d5ad9db95b574a1ce
kujiro <kantake1969@gmail.com>
2021-12-10 15:08:43 +0900

    ビルド反映

--- docs/_content/WebResource/WorkerParent.js
+++ docs/_content/WebResource/WorkerParent.js
@@ -16,18 +16,27 @@ import JSTextDecoder from "./TextDecoder.js";
 const workers = [];
 
 /**
+ * @private
+ * @type {Int32Array[]}
+ * */
+const buffers = [];
+
+/**
  * Configure this script.
- * @param {number} ptr
- * @param {number} len
- * @returns {void}
+ * @param {number} jsonPtr
+ * @param {number} jsonLen
+ * @returns {number}
  */
-export function Configure(ptr, len) {
+export function Configure(jsonPtr, jsonLen, bufferLen) {
     /** @type EnvironmentSettings */
-    const data = DecodeUTF8JSON(ptr, len);
+    const data = DecodeUTF8JSON(jsonPtr, jsonLen);
     workerScriptUrl = data.WorkerScriptPath
     dotnetAssemblyName = data.AssemblyName;
     dotnetMessageEventHandler = data.MessageHandlerName;
     dotnetInitializedHandler = data.InitializedHandlerName;
+
+    const buffer = Module._malloc(bufferLen);
+    return buffer;
 }
 
 /** @type {string} */
@@ -57,7 +66,35 @@ export function CreateWorker(ptr, len) {
     const array2 = new Uint8Array(array);
     worker.postMessage([array2.buffer], [array2.buffer]);
     workers.push(worker);
+    buffers.push(undefined);
     return index;
+}
+
+/**
+ * Allocate buffer to call worker 
+ * @param {any} id worker id
+ * @param {any} len buffer length in byte
+ * @returns {number} buffer address or null pointer(0).
+ */
+export function AllocBuffer(id, len) {
+    if (buffers[id] === undefined) {
+        const addr = Module._malloc(len);
+        const array = new Int32Array(wasmMemory.buffer, addr, len / 4);
+        buffers[id] = array;
+        return addr;
+    } else {
+        return 0;
+    }
+}
+
+export function SCall(workerId, len, callId) {
+    if (len < 16) {
+        throw new Error();
+    }
+    const buffer = buffers[workerId];
+    const methodName = wasmMemory.buffer.slice(buffer[0], buffer[0] + buffer[1]);
+    const jsonBin = wasmMemory.buffer.slice(buffer[2], buffer[2] + buffer[3]);
+    workers[workerId].postMessage({ t: "SCall", d: [methodName, jsonBin] }, [methodName, jsonBin]);
 }
 
 /**
@@ -93,14 +130,4 @@ function DecodeUTF8JSON(ptr, len) {
     const array = new Uint8Array(wasmMemory.buffer, ptr, len);
     const str = len > nativeLen ? nativeDecoder.decode(array) : JSTextDecoder(array);
     return JSON.parse(str);
-}
-
-/**
- * Calls static method on worker.
- * @param {Number} id worker id;
- * @param {string} method method(full name) to call.
- * @returns {void}
- */
-export function _Call(id, method) {
-    workers[id].postMessage(method);
 }
